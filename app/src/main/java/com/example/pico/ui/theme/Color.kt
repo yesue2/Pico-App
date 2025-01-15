3e368a263bec1e954c8c81f0638172b65cdbe22a
YeSeul <ysbsjh2464@naver.com>
2025-01-15 20:43:37 +0900

    :lipstick: design: 앱 상태 표시줄 테마(색) 수정

--- app/src/main/java/com/example/pico/ui/theme/Color.kt
+++ app/src/main/java/com/example/pico/ui/theme/Color.kt
@@ -9,3 +9,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Yellow100 = Color(0xFFF99600)
+val Yellow90 = Color(0xFFFFC700)
+val Yellow80 = Color(0xFFFFD613)
+val Yellow70 = Color(0xFFFFE45F)
+val Yellow60 = Color(0xFFFFE582)
+val Yellow50 = Color(0xFFFFECA3)
+val Yellow40 = Color(0xFFFFF4C9)
+val Yellow30 = Color(0xFFFFF9E1)
+
+val MainYellow = Color(0xFFFFBB38)
+val MainDarkBrown = Color(0xFF534B3C)
+
--- app/src/main/java/com/example/pico/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pico/ui/theme/Theme.kt
@@ -10,21 +10,22 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Yellow90,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Yellow90,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -40,11 +41,10 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun PicoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+//    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+/*    val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -52,13 +52,15 @@ fun PicoTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
+    }*/
+
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.Transparent.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
