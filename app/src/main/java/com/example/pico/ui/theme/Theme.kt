bd9ccc8fae419cf24115b657178bf9c268bdaf6d
YeSeul <ysbsjh2464@naver.com>
2025-01-17 23:30:24 +0900

    :lipstick: design: 상태 표시줄, 하단 네비게이션바 테마 색상 수정

--- app/src/main/java/com/example/pico/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pico/ui/theme/Theme.kt
@@ -1,18 +1,14 @@
 package com.example.pico.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -60,8 +56,8 @@ fun PicoTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
-            window.navigationBarColor = Color.Transparent.toArgb()
+            window.statusBarColor = colorScheme.tertiary.toArgb()
+            window.navigationBarColor = colorScheme.tertiary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
