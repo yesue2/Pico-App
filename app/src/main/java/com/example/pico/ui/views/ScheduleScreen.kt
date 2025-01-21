d8a35611dfea3fb228968d29f0927a6150ad1dd3
YeSeul <ysbsjh2464@naver.com>
2025-01-22 08:07:47 +0900

    :lipstick: design: schedule UI 구현 추가

---
+++ app/src/main/java/com/example/pico/ui/views/ScheduleScreen.kt
@@ -0,0 +1,79 @@
+package com.example.pico.ui.views
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.pico.ui.components.BottomAppBar
+import com.example.pico.ui.components.TaskList
+import com.example.pico.ui.components.TopAppBar
+import com.example.pico.ui.theme.PicoTheme
+
+@Composable
+fun ScheduleScreen(navController: NavController) {
+    Scaffold(
+        topBar = { TopAppBar(screen = "Schedule") },
+        bottomBar = { BottomAppBar(navController = navController) }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            item {
+                TodoListSection()
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoListSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = "할 일 목록",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TaskList()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Preview(
+    showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark"
+)
+@Composable
+fun TaskListPreview() {
+    val navController = rememberNavController()
+    PicoTheme {
+        ScheduleScreen(navController)
+    }
+}
