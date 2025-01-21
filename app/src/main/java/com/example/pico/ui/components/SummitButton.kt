5668c454348943768ae7daa888f5afcdb5dfda5b
YeSeul <ysbsjh2464@naver.com>
2025-01-22 08:14:20 +0900

    :lipstick: design: 메인 버튼 색상 테마 설정

--- app/src/main/java/com/example/pico/ui/components/SummitButton.kt
+++ app/src/main/java/com/example/pico/ui/components/SummitButton.kt
@@ -5,11 +5,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -38,7 +38,7 @@ fun SummitButton(content: String, onClick: () -> Unit) {
 
         Text(
             text = content,
-            color = Color.White,
+            color = MaterialTheme.colorScheme.tertiary,
             fontSize = 23.sp,
             fontWeight = FontWeight.Bold
         )
